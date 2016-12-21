98106330e7855cd9fc6bcc235627caf93dce1866
Santosh Addanki <santosh.kumar.addanki@sap.com>
2016-12-21 15:02:21 -0800

    Random Changes

--- src/main/scala/MethodWithFunctionParameter_93.scala
+++ src/main/scala/MethodWithFunctionParameter_93.scala
@@ -1,4 +1,16 @@
 
 object MethodWithFunctionParameter_93 extends App {
 
+  def executeFunction(callback:() => Unit){
+
+    callback()
+  }
+
+  val sayHello = () => println("Hello World")
+
+  executeFunction(sayHello)
+
+  //general syntax
+  // parameterName:(parameterType(s)) => returnType
+
 }
